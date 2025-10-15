37b63046d28a9180cad63ce8ff457b4873b64d9e
Matt Keeter <matt@oxide.computer>
2025-10-15 19:50:05 +0000

    DLPI debugging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use anyhow::Result;
-use bootserver::dlpi::{Address, Dlpi};
+use anyhow::{Result, anyhow, bail};
+use bootserver::dlpi;
+use pnet::packet::ethernet::EtherTypes;
+use pnet::packet::icmpv6::MutableIcmpv6Packet;
+use pnet::packet::icmpv6::{Icmpv6Packet, Icmpv6Types, echo_request};
+use pnet::packet::ipv6::MutableIpv6Packet;
 use std::env;
+use std::net::Ipv6Addr;
 use std::thread;
-
-const DLSEND_SAP: u32 = 0xdeed;
-const DLSEND_MSG: &str = "A Elbereth Gilthoniel";
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -13,30 +15,170 @@ fn main() -> Result<()> {
         std::process::exit(1);
     }
 
-    let interface_name = args[1].to_owned();
+    let interface_name = &args[1];
 
     // Find the network interface
-    let mut iface_send = Dlpi::open(&interface_name)?;
-    iface_send.bind_ethertype(DLSEND_SAP)?;
+    let interfaces = pnet::datalink::interfaces();
+    let iface = match interfaces.iter().find(|i| i.name == *interface_name) {
+        Some(i) => i,
+        None => {
+            eprintln!("Interface '{}' not found", interface_name);
+            std::process::exit(1);
+        }
+    };
+    let ipv6 = iface
+        .ips
+        .iter()
+        .find_map(|ip| match ip.ip() {
+            std::net::IpAddr::V4(_) => None,
+            std::net::IpAddr::V6(ip) => {
+                if ip.is_unicast_link_local() {
+                    Some(ip)
+                } else {
+                    None
+                }
+            }
+        })
+        .ok_or_else(|| anyhow!("could not get IP address"))?;
 
-    let rx_handle = thread::spawn(move || -> Result<()> {
-        let mut iface_recv = Dlpi::open(&interface_name)?;
-        iface_recv.bind_ethertype(DLSEND_SAP)?;
-        for _ in 0..100 {
-            if let Ok(Some(packet)) = iface_recv.recv(Some(100)) {
+    // Link-local addresses in EUI-64 format have the MAC address embedded
+    // in segments 4-6 (bytes 8-15): fe80:0000:0000:0000:XXYY:ZZFF:FEWW:VVUU
+    // where XX-YY-FF-FE-WW is inserted by the EUI-64 process
+    // Original MAC: XX-YY-ZZ-WW-VV-UU
+    let segments = ipv6.segments();
+
+    // Extract bytes 8-15 (segments 4-7)
+    let byte1 = ((segments[4] >> 8) as u8) ^ 0x02; // Flip the U/L bit
+    let byte2 = (segments[4] & 0xFF) as u8;
+    let byte3 = (segments[5] >> 8) as u8;
+    // segments[5] & 0xFF should be 0xFF
+    // segments[6] >> 8 should be 0xFE
+    let byte4 = (segments[6] & 0xFF) as u8;
+    let byte5 = (segments[7] >> 8) as u8;
+    let byte6 = (segments[7] & 0xFF) as u8;
+
+    let mac = dlpi::Address {
+        addr: [byte1, byte2, byte3, byte4, byte5, byte6],
+    };
+
+    println!("Testing interface: {}", iface.name);
+    println!("MAC address: {:02x?}", mac.addr);
+
+    let frame = construct_icmpv6_frame(
+        ipv6,
+        Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0x01de, 2), // multicast
+        &[1; 56],
+        123, // identifier
+        456, // sequence
+    )?;
+
+    let (tx, rx) = std::sync::mpsc::channel();
+    let interface_name_ = interface_name.to_owned();
+    let frame_ = frame.clone();
+    let rx_handle = thread::spawn(move || -> Result<bool> {
+        let mut iface_recv = dlpi::Dlpi::open(&interface_name_)?;
+        iface_recv.bind_ethertype(u32::from(EtherTypes::Ipv6.0))?;
+        iface_recv.promisc_on()?;
+        tx.send(()).unwrap(); // unblock the main thread
+        let end_time = std::time::Instant::now() + std::time::Duration::from_millis(100);
+        while std::time::Instant::now() < end_time {
+            if let Some(packet) = iface_recv.recv(Some(100))? {
                 println!(
-                    "got packet from {:?} to {:?} with data {:#x?}",
+                    "got packet from {:02x?} to {:02x?} with data {:x?}",
                     packet.src().map(|p| p.addr),
                     packet.dst().map(|p| p.addr),
                     packet.data(),
                 );
+                if packet.data() == frame_ {
+                    return Ok(true);
+                }
             }
         }
-        Ok(())
+        println!("timeout");
+        Ok(false)
     });
 
-    iface_send.send(Address { addr: [0xFF; 6] }, DLSEND_MSG.as_bytes())?;
-    rx_handle.join().unwrap().unwrap();
+    rx.recv().unwrap();
+
+    // Find the network interface
+    let mut iface_send = dlpi::Dlpi::open(interface_name)?;
+    iface_send.bind_ethertype(u32::from(EtherTypes::Ipv6.0))?;
+
+    iface_send.send(
+        dlpi::Address {
+            addr: [0x33, 0x33, 0x1, 0xde, 0x0, 0x2], // multicast
+        },
+        &frame,
+    )?;
+    let out = rx_handle.join().unwrap()?;
+    if out {
+        println!("found loopback, great job");
+    } else {
+        bail!("no loopback detected");
+    }
 
     Ok(())
+}
+
+pub fn construct_icmpv6_frame(
+    src_ipv6: Ipv6Addr,
+    dest_ipv6: Ipv6Addr,
+    data: &[u8; 56],
+    identifier: u16,
+    sequence: u16,
+) -> Result<Vec<u8>> {
+    // Calculate sizes
+    const ICMPV6_HEADER_SIZE: usize = 8;
+    const IPV6_HEADER_SIZE: usize = 40;
+
+    let icmpv6_payload_size = ICMPV6_HEADER_SIZE + data.len();
+    let total_size = IPV6_HEADER_SIZE + icmpv6_payload_size;
+
+    // Create buffer for the entire frame
+    let mut buffer = vec![0; total_size];
+
+    // ===== Construct ICMPv6 Echo Request =====
+    let icmpv6_offset = IPV6_HEADER_SIZE;
+    {
+        let mut icmpv6_packet = MutableIcmpv6Packet::new(&mut buffer[icmpv6_offset..]).unwrap();
+        icmpv6_packet.set_icmpv6_type(Icmpv6Types::EchoRequest);
+        icmpv6_packet.set_icmpv6_code(pnet::packet::icmpv6::Icmpv6Code(0));
+    }
+
+    // Create echo request at correct offset
+    {
+        let mut echo_req =
+            echo_request::MutableEchoRequestPacket::new(&mut buffer[icmpv6_offset..]).unwrap();
+        echo_req.set_identifier(identifier);
+        echo_req.set_sequence_number(sequence);
+        echo_req.set_payload(data);
+    }
+
+    // Calculate and set ICMPv6 checksum
+    let checksum = {
+        let icmpv6_immutable = Icmpv6Packet::new(&buffer[icmpv6_offset..]).unwrap();
+        pnet::packet::icmpv6::checksum(&icmpv6_immutable, &src_ipv6, &dest_ipv6)
+    };
+
+    {
+        let mut icmpv6_packet = MutableIcmpv6Packet::new(&mut buffer[icmpv6_offset..]).unwrap();
+        icmpv6_packet.set_checksum(checksum);
+    }
+
+    // ===== Construct IPv6 Header =====
+    {
+        let ipv6_offset = 0;
+        let mut ipv6_packet = MutableIpv6Packet::new(&mut buffer[ipv6_offset..]).unwrap();
+        ipv6_packet.set_version(6);
+        ipv6_packet.set_traffic_class(0);
+        ipv6_packet.set_flow_label(0);
+        ipv6_packet.set_payload_length(icmpv6_payload_size as u16);
+        ipv6_packet.set_next_header(pnet::packet::ip::IpNextHeaderProtocols::Icmpv6);
+        ipv6_packet.set_hop_limit(64);
+        ipv6_packet.set_source(src_ipv6);
+        ipv6_packet.set_destination(dest_ipv6);
+    }
+
+    // XXX ahhhh
+    Ok(buffer)
 }
